8c19e992e53380b17b3ae5699ac6b690860cf40d
Christopher Suárez <caso03@yahoo.com>
2024-11-08 14:37:55 -0600

    Improve stripHTMLTags function

--- src/lib/appService.ts
+++ src/lib/appService.ts
@@ -1,7 +1,7 @@
 class AppService {
 	stripHTMLTags(html: string) {
 		return html
-			.replace(/<\/?(div|p|i|span|strong|b)[^>]*>/gi, '') // Remove specified tags
+			.replace(/<\/?(div|p|i|span|strong|a|br)[^>]*>/gi, '') // Remove HTML tags
 			.replace(/\s+/g, ' ') // Replace multiple spaces with a single space
 			.trim(); // Trim leading and trailing spaces
 	}
