529ba0c3a33d59fab332405429d633b3af7434e8
Christopher Suárez <caso03@yahoo.com>
2025-02-04 14:46:57 -0600

    chore: update dependencies to latest supported by Svelte 4

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true, // Silence deprecation warnings from dependencies
+				silenceDeprecations: ['import'], // Migration to `@use` is unnecessary since `@import` is only used in the root `src/styles/main.scss` file and the requested files will be imported only once.
+				api: 'modern', // Use the modern API instead of the legacy JavaScript API
+			},
+		},
+	},
 });
